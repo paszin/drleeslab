2e4abef582261b823a348e57ea8c0dc4c658ad0c
Jaeyoon Jung <jaeyoonjung85@gmail.com>
2016-04-23 16:08:16 -0700

    import re

--- python_server/hello.py
+++ python_server/hello.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import urllib2
+import re
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,11 +9,28 @@ def hello_world():
 
 @app.route('/correlated_queries/<event>')
 def getCorrelatedQueries(event):
-	url="http://google.com"
-	page =urllib2.urlopen(url)
+	url1 = "https://www.google.com/trends/correlate/search?e="
+	url2 = "&t=weekly&p=us"
+	event = event.replace(" ", "+")
+	page =urllib2.urlopen(url1 + event + url2)
 	data=page.read()
-	print data
-	return event
+
+@app.route('/correlated_queries/<event>/<correlated_query>')
+def getCorrelatedQueriesPlots(event):
+	# url1="https://www.google.com/trends/correlate/search?e=fukushima+tsunami&t=weekly&p=us"
+	url1 = "https://www.google.com/trends/correlate/search?e="
+	url2 = "&t=weekly&p=us"
+	event = event.replace(" ", "+")
+	page =urllib2.urlopen(url1 + event + url2)
+	data=page.read()
+	startIndex = data.index("series_set = ") + 13
+	semiColonIndexes = [m.start() for m in re.finditer(';', data)]
+	for thisIndex in semiColonIndexes:
+		if (thisIndex > startIndex):
+			endIndex = thisIndex
+			break
+	print data[startIndex:endIndex]
+	return data[startIndex:endIndex]
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
