7f8a0e43f8df32cde54e63b80c58e10eb5fcbea6
Yao Liu <liuyaoly@umich.edu>
2016-04-23 18:21:29 -0700

    landing

--- client/app/landing/landing.controller.js
+++ client/app/landing/landing.controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
-angular.module('spaceappsApp')
-  .controller('LandingCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+var map_basic = angular.module('spaceappsApp');
+
+map_basic.controller('LandingCtrl',[ '$scope', function ($scope) {
+    //  $scope.message = 'Hello';
+     angular.extend($scope, {
+          center: {
+              lat: 51.505,
+              lng: -0.09,
+              zoom: 4
+          },
+          successfactor: {
+            lat: 37.668443,
+            lng: -122.4495878,
+            zoom: 12
+          }
+      });
+  }]);
