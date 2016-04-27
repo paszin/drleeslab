9e16d390de0ae963d1dc815d262ab0524bc5671f
Pascal <pascalslogin@gmail.com>
2016-04-27 00:18:04 -0700

    added facebook integration

--- client/app/app.js
+++ client/app/app.js
@@ -9,7 +9,8 @@ angular.module('spaceappsApp', [
   'ui.router',
   'ngMaterial',
   'leaflet-directive', // including leaflet.js library.
-  'nvd3'
+  'nvd3',
+  'ngFacebook'
 ])
   .config(function($mdIconProvider) {
     $mdIconProvider
@@ -36,4 +37,27 @@ angular.module('spaceappsApp', [
       .otherwise('/landing');
 
     $locationProvider.html5Mode(true);
-  });
+  })
+  .config( function( $facebookProvider ) {
+  $facebookProvider.setAppId('1721217284819855');
+  $facebookProvider.setPermissions("email,user_education_history,user_tagged_places,user_hometown,user_work_history,user_location,user_friends,read_custom_friendlists");
+}).run( function( $rootScope ) {
+  // Load the facebook SDK asynchronously
+  (function(){
+     // If we've already installed the SDK, we're done
+     if (document.getElementById('facebook-jssdk')) {return;}
+
+     // Get the first script element, which we'll use to find the parent node
+     var firstScriptElement = document.getElementsByTagName('script')[0];
+
+     // Create a new script element and set its id
+     var facebookJS = document.createElement('script'); 
+     facebookJS.id = 'facebook-jssdk';
+
+     // Set the new script's source to the source of the Facebook JS SDK
+     facebookJS.src = '//connect.facebook.net/en_US/all.js';
+
+     // Insert the Facebook JS SDK into the DOM
+     firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
+   }());
+});
