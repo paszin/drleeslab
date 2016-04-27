2006f5f3f2b5cd89d375648fdc270f54a2bcc6e5
Pascal <pascalslogin@gmail.com>
2016-04-27 01:21:11 -0700

    added facebook login to view to find friends

--- client/app/landing/landing.controller.js
+++ client/app/landing/landing.controller.js
@@ -1,118 +1,179 @@
-'use strict';
+/*global angular, console*/
+var app = angular.module('spaceappsApp');
 
-var map_basic = angular.module('spaceappsApp');
+app.controller('LandingCtrl', ['$scope', '$rootScope', '$q', '$http', '$location', '$facebook', 'mapBaselayers',
+                            function ($scope, $rootScope, $q, $http, $location, $facebook, mapBaselayers) {
+        'use strict';
 
-map_basic.controller('LandingCtrl',[ '$scope', '$http', '$location', function ($scope, $http, $location) {
-    $scope.searchWindow = 200; //days
     
-    var iconLookup = {'Floods': 'flood.svg', 'Severe Storms': 'tornado.svg', 'Wildfires': 'wildfire.svg', 'Dust and Haze': 'house_fire.svg', 'Water Color': 'tsunami.svg', 'Volcanoes': "volcano.svg", 'Sea and Lake Ice': 'ice-seae.svg'};
-    
-    var earthquakeUrl = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
-    //last ten days
-    $http.get(earthquakeUrl).success(function(response) {
-        var points =  response.features.map(function(d) {
-          return {
-            layer: "earthquake",
-            lat: d.geometry.coordinates[1],
-            lng: d.geometry.coordinates[0],
-            message: d.id
-          };
-        });
-                                               
-        console.log("Earthquakes", points.length);
-      
-        
-    });
-    
-     $scope.$on('leafletDirectiveMarker.mymap.click', function (e, args) {
-        $scope.info = args.model.message;
-        $location.path('/event');
-        $location.search('link', args.model.data.link);
-        });
-    
-    $scope.$on('leafletDirectiveMarker.mymap.mouseover', function (e, args) {
-        //debugger;
-        args.leafletObject.openPopup();
-                $scope.info = args.model.data.title;
-        });
-    
-    
-    //get events
-    $scope.openEvents = [];
-    
-    $http.get('http://eonet.sci.gsfc.nasa.gov/api/v2.1/events?status=open&limit=900&days=5000').success(function(api) {
-        $scope.openEventsList = api.events;
-      api.events.forEach(function(natureevent) {
-          var lat, lng;
-          if (!!natureevent.geometries && natureevent.geometries.length > 0 && !!natureevent.geometries[0].coordinates && natureevent.geometries[0].coordinates.length > 0) {
+        // FACEBOOK
+        $rootScope.user = $rootScope.user || {};
+        $rootScope.isLoggedIn = false;
+        $scope.paths = {};
+        var fbFriendsLocations = "/me/friends?fields=address,location,name,picture,hometown&limit=1000",
+            fbPersonalData = "/me?fields=location,hometown,picture";
 
-              if (natureevent.geometries[0].type === "Polygon") {
-                  lat = natureevent.geometries[0].coordinates[0][0][1]; 
-                  lng = natureevent.geometries[0].coordinates[0][0][0];
-              } else if (natureevent.geometries[0].type === "Point") {
-                  lat = natureevent.geometries[0].coordinates[1]; 
-                  lng = natureevent.geometries[0].coordinates[0]; 
-            }
-              $scope.openEvents.push({data: natureevent, message: natureevent.title, layer: "natureevents", lat: lat, lng: lng, icon: //{}
-                                      {
-                    iconUrl: 'assets/icons/' + iconLookup[natureevent.categories[0].title] || 'assest/icons/dots-vertical.svg',
-                    //shadowUrl: 'img/leaf-shadow.png',
-                    iconSize:     [24, 24], // size of the icon
-                    shadowSize:   [50, 64], // size of the shadow
-                    iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
-                    shadowAnchor: [0, 0],  // the same for the shadow
-                    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
-                }
-                
-            });    
-          }
-      });
-        
-    });
-
-    angular.extend($scope, {
-        center: {
-          lat: 0,
-          lng: 0,
-          zoom: 2
-        },
-        cloudIcon: {
-                    iconUrl: 'assets/images/cloud.png',
-                    //shadowUrl: 'img/leaf-shadow.png',
-                    iconSize:     [38, 38], // size of the icon
-                    shadowSize:   [50, 64], // size of the shadow
-                    iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
-                    shadowAnchor: [4, 62],  // the same for the shadow
-                    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-                },
-        markers: $scope.openEvents,
-        layers: {
-          baselayers: {
-            osm: {
-              name: 'OpenStreetMap',
-              url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-              type: 'xyz'
-            },
-          },
-          overlays:{
-            natureevents: {
-            name: 'natureevents',
-            type: 'group',
-            visible: true
-            },
-              truecolor: {
-                  name: "Sat",
-                  url: "http://map1{s}.vis.earthdata.nasa.gov/wmts-geo/" +
-        "MODIS_Terra_CorrectedReflectance_TrueColor/default/2013-11-04/EPSG4326_250m/{z}/{y}/{x}.jpg",
-                  type: "xyz"
-              } 
-              /*,something: {
-                  name: "iydfguladfh",
-                  type: "xyz",
-                  url: "http://map1{s}.vis.earthdata.nasa.gov/wmts-arctic/MODIS_Aqua_CorrectedReflectance_TrueColor/default/2013-06-20/EPSG3413_250m/{z}/{y}/{x}.jpg"
-              }*/
-          }
+        function getFbFriendsLocation() {
+            $facebook.api(fbFriendsLocations).then(function (response) {
+                response.data.forEach(function (entry) {
+                    getCoordinates(entry.location.id);
+                });
+                $scope.friends = response.data;
+            }, function (response) {
+                console.warn("error fetching friends data", response);
+            });
         }
-      });
-    
+
+        function getFbPersonalData() {
+            $facebook.api(fbPersonalData).then(function (response) {
+                $rootScope.user = response;
+                $rootScope.isLoggedIn = true;
+            }, function (err) {
+                console.warn("ERROR DURING FACEBOOK LOGIN", err);
+            });
+        }
+
+        function getCoordinates(id) {
+            $facebook.api(id + "?fields=location").then(function (data) {
+                $scope.paths[id] = {
+                        weight: 2,
+                        color: '#2f61ff',
+                        latlngs: {
+                            lat: data.location.latitude,
+                            lng: data.location.longitude
+                        },
+                        radius: 200000,
+                        type: 'circle',
+                        layer: 'shapes',
+                stroke: false
+                    }
+            });
+        }
+        //login
+        $scope.login = function () {
+            $facebook.login().then(function () {
+                getFbPersonalData();
+                getFbFriendsLocation();
+            });
+        };
+                                
+        $scope.logout = function () {
+            $facebook.logout().then(function (resp) {
+                console.log(resp);
+            });
+        };
+
+
+        //NATURE EVENTS
+        $scope.openEvents = [];
+        var earthquakeUrl = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson",
+            natureEventsOpenUrl = "http://eonet.sci.gsfc.nasa.gov/api/v2.1/events?status=open&limit=900&days=5000";
+        //earthquakes
+        $http.get(earthquakeUrl).success(function (response) {
+            var points =  response.features.map(function (d) {
+                return {
+                    layer: "earthquake",
+                    lat: d.geometry.coordinates[1],
+                    lng: d.geometry.coordinates[0],
+                    message: d.id
+                  };
+            });
+            console.log("Earthquakes", points.length); 
+        });
+         
+        //nature events eonet
+        $http.get(natureEventsOpenUrl).success(function(api) {
+            $scope.openEventsList = api.events;
+            api.events.forEach(function(natureevent) {
+            var lat, lng;
+            //just take one of the coordinates, room for improvement!
+            if (!!natureevent.geometries && natureevent.geometries.length > 0 && !!natureevent.geometries[0].coordinates && natureevent.geometries[0].coordinates.length > 0) {
+
+                  if (natureevent.geometries[0].type === "Polygon") {
+                      lat = natureevent.geometries[0].coordinates[0][0][1]; 
+                      lng = natureevent.geometries[0].coordinates[0][0][0];
+                  } else if (natureevent.geometries[0].type === "Point") {
+                      lat = natureevent.geometries[0].coordinates[1]; 
+                      lng = natureevent.geometries[0].coordinates[0]; 
+                }
+                  $scope.openEvents.push({data: natureevent, message: natureevent.title, layer: "natureevents", lat: lat, lng: lng, icon: //{}
+                                          {
+                        iconUrl: 'assets/icons/' + iconLookup[natureevent.categories[0].title] || 'assest/icons/dots-vertical.svg',
+                        //shadowUrl: 'img/leaf-shadow.png',
+                        iconSize:     [24, 24], // size of the icon
+                        shadowSize:   [50, 64], // size of the shadow
+                        iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
+                        shadowAnchor: [0, 0],  // the same for the shadow
+                        popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+                    }
+                });    
+              }
+          });
+
+        });
+
+
+
+
+        //MAP CONFIG
+        var iconLookup = {'Floods': 'flood.svg', 'Severe Storms': 'tornado.svg', 'Wildfires': 'wildfire.svg', 'Dust and Haze': 'house_fire.svg', 'Water Color': 'tsunami.svg', 'Volcanoes': "volcano.svg", 'Sea and Lake Ice': 'ice_sea.svg'};
+
+
+         $scope.$on('leafletDirectiveMarker.mymap.click', function (e, args) {
+            $scope.info = args.model.message;
+            $location.path('/event');
+            $location.search('link', args.model.data.link);
+            });
+
+        $scope.$on('leafletDirectiveMarker.mymap.mouseover', function (e, args) {
+            //debugger;
+            args.leafletObject.openPopup();
+                    $scope.info = args.model.data.title;
+            });
+                                
+         $scope.$on('leafletDirectivePath.mymap.mouseover', function (e, args) {
+            //debugger;
+            args.leafletObject.openPopup();
+                    $scope.info = args.model.data.title;
+            });
+                                
+                                
+                        
+
+        angular.extend($scope, {
+            center: {
+              lat: 0,
+              lng: 0,
+              zoom: 2
+            },
+            markers: $scope.openEvents,
+            paths: $scope.paths,
+            layers: {
+              baselayers: mapBaselayers,
+              overlays:{
+                natureevents: {
+                name: 'natureevents',
+                type: 'group',
+                visible: true
+                },
+                  truecolor: {
+                      name: "Sat",
+                      url: "http://map1{s}.vis.earthdata.nasa.gov/wmts-geo/" +
+            "MODIS_Terra_CorrectedReflectance_TrueColor/default/2013-11-04/EPSG4326_250m/{z}/{y}/{x}.jpg",
+                      type: "xyz"
+                  } 
+                  ,something: {
+                      name: "earthdata",
+                      type: "xyz",
+                      url: "http://map1{s}.vis.earthdata.nasa.gov/wmts-arctic/MODIS_Aqua_CorrectedReflectance_TrueColor/default/2013-06-20/EPSG3413_250m/{z}/{y}/{x}.jpg"
+                  },
+                  shapes: {
+                            name: 'Shapes',
+                            type: 'group',
+                            visible: true
+                        }
+              }
+            }
+          });
+
   }]);
--- client/app/landing/landing.js
+++ client/app/landing/landing.js
@@ -8,4 +8,18 @@ angular.module('spaceappsApp')
         templateUrl: 'app/landing/landing.html',
         controller: 'LandingCtrl'
       });
-  });
+  }).constant('mapBaselayers', {
+                osm: {
+                  name: 'OpenStreetMap',
+                  url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  type: 'xyz'
+                },
+                bingAerial: {
+                    name: 'Bing Aerial',
+                    type: 'bing',
+                    key: 'Au7WV84YO2KuKIhX2Bc-xBLFzeHzZwpNjwwQo5b9pdHT-DX2Gh_4EQyZ6Q9vyWQ8',
+                    layerOptions: {
+                        type: 'Aerial'
+                    }
+                },
+            });
