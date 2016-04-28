002e4d69f65f4fdfad04bbf98282822a1109ad31
Pascal <pascalslogin@gmail.com>
2016-04-27 17:52:35 -0700

    Added twitter v1

--- client/components/dashboard/twitterFeed/twitterFeed.directive.js
+++ client/components/dashboard/twitterFeed/twitterFeed.directive.js
@@ -4,8 +4,13 @@ angular.module('spaceappsApp')
   .directive('twitterFeed', function () {
     return {
       templateUrl: 'components/dashboard/twitterFeed/twitterFeed.html',
-      restrict: 'EA',
+      restrict: 'E',
+      scope: {
+          'tweetids': '=tweetids',
+          'data': '=data'
+      },
       link: function (scope, element, attrs) {
+          scope.ids = [722389353218064384];
       }
     };
   });
