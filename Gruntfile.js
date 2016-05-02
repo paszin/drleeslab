24e7375236ff342f5a9a8105d3eb45e4d72057b5
Pascal <pascalslogin@gmail.com>
2016-05-01 22:12:52 -0700

    fixed gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -309,7 +309,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       options: {
         // This should be the name of your apps angular module
-        module: 'goaldenAppApp',
+        module: 'spaceappsApp',
         htmlmin: {
           collapseBooleanAttributes: true,
           collapseWhitespace: true,
